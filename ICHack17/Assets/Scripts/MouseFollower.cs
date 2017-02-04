f9126fcfe5ee6885077bfe0db01b8e1e99d6e049
Harjuno Perwironegoro <hperwironegoro@gmail.com>
2017-02-04 13:42:58 +0000

    [HP] Added the initial assets

---
+++ ICHack17/Assets/Scripts/MouseFollower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollower : MonoBehaviour {
+
+	void Update () {
+        var v3 = Input.mousePosition;
+        v3.z = 10.0f;
+        v3 = Camera.main.ScreenToWorldPoint(v3);
+
+        transform.position = v3;
+    }
+}
