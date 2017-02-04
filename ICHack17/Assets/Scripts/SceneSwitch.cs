ebdac9816bdb777f5105ac5c2a2be7c67849e2a5
Harjuno Perwironegoro <hperwironegoro@gmail.com>
2017-02-04 23:48:43 +0000

    [HP] Added scene switching.

---
+++ ICHack17/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class SceneSwitch : MonoBehaviour {
+    public string nextSceneName;
+
+    public void NextScene() {
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
