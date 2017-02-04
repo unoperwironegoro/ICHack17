e1864d61bd81e3bdd519d0bc28a9ec50dd848308
Nimesh Subedi <Nimesh Subedi>
2017-02-04 20:34:01 +0000

    [NS] Added sumoFollower script.

---
+++ ICHack17/Assets/Scripts/sumoFollower.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class sumoFollower : NetworkBehaviour {
+
+    public float speed;
+
+    public override void OnStartLocalPlayer()
+    {
+     /*   Color color = Random.ColorHSV();
+        color.a = 1f;
+
+        GetComponent<SpriteRenderer>().color = color;
+        CmdSetColor(color); 
+        */
+    }
+
+    [Command]
+    void CmdSetColor(Color color)
+    {
+        GetComponent<SpriteRenderer>().color = color;
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        float step = speed * Time.deltaTime;
+
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10.0f;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        Vector3 playerPos = transform.position;
+
+        if (Vector3.Distance(mousePos, playerPos) < 2)
+        {
+            step /= 2;
+        }
+
+        transform.position = Vector3.MoveTowards(playerPos, mousePos, step);
+    }
+}
