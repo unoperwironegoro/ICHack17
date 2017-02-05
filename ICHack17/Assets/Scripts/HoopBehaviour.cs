5a5e329e0a5436d9ce5b44cafb83786d52feefb1
Jay <jrm315@ic.ac.uk>
2017-02-05 01:42:47 +0000

    [JM] Basketball game needs a background and a score display but otherwise should work.

---
+++ ICHack17/Assets/Scripts/HoopBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoopBehaviour : MonoBehaviour {
+
+    private int redCount;
+    private int greenCount;
+    private int blueCount;
+    private int orangeCount;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter (Collider other) {
+        string tag = other.tag;
+        switch (tag) {
+            case "Red":
+                redCount++; break;
+            case "Green":
+                greenCount++; break;
+            case "Orange":
+                orangeCount++; break;
+            case "Blue":
+                blueCount++; break;            
+        }
+        
+    }
+}
