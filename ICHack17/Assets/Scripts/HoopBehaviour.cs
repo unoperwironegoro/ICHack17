6bcc62d63ed28872acc6b4d2e3f3bc2b8cb3ebe6
Jay <jrm315@ic.ac.uk>
2017-02-05 02:29:21 +0000

    Need to implement colisions

--- ICHack17/Assets/Scripts/HoopBehaviour.cs
+++ ICHack17/Assets/Scripts/HoopBehaviour.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+
 
 public class HoopBehaviour : MonoBehaviour {
 
-    private int redCount;
-    private int greenCount;
-    private int blueCount;
-    private int orangeCount;
+    private int redCount = 0;
+    private int greenCount = 0;
+    private int blueCount = 0;
+    private int orangeCount = 0;
+
+    public Text scoreDisplay;
 
     // Use this for initialization
     void Start () {
-        
+        setDisplay ();
     }
 	
 	// Update is called once per frame
@@ -19,18 +23,25 @@ public class HoopBehaviour : MonoBehaviour {
 		
 	}
 
-    void OnTriggerEnter (Collider other) {
-        string tag = other.tag;
-        switch (tag) {
-            case "Red":
-                redCount++; break;
-            case "Green":
-                greenCount++; break;
-            case "Orange":
-                orangeCount++; break;
-            case "Blue":
-                blueCount++; break;            
+    void OnTriggerEnter2D(Collider2D obj) {
+
+        if (obj.gameObject.CompareTag("Red")) {
+            redCount++;
+        } else if (obj.gameObject.CompareTag("Green")) {
+            greenCount++;
+        } else if (obj.gameObject.CompareTag("Orange")) {
+            orangeCount++;
+        } else { 
+            blueCount++; 
         }
+        setDisplay();
         
+    }
+
+    void setDisplay () {
+        scoreDisplay.text = "Red:" + redCount.ToString() +
+                           " Blue: " + blueCount.ToString() +
+                           " Green: " + greenCount.ToString() +
+                           " Orange: " + orangeCount.ToString();
     }
 }
