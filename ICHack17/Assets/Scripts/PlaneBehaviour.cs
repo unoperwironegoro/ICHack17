f9fc844f9db559c022435eb04e66ae2e75a524ef
Harjuno Perwironegoro <hperwironegoro@gmail.com>
2017-02-05 11:34:57 +0000

    Server check for bullets.

--- ICHack17/Assets/Scripts/PlaneBehaviour.cs
+++ ICHack17/Assets/Scripts/PlaneBehaviour.cs
@@ -80,6 +80,10 @@ public class PlaneBehaviour : NetworkBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if(!isServer) {
+            return;
+        }
+
         if (collision.gameObject.GetComponent<bulletBehaviour>().owner == gameObject)
         {
             return;
