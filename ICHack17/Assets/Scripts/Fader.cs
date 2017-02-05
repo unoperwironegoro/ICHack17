d58a9af166b1bf1ac4c32fede94dd26c56133103
Harjuno Perwironegoro <hperwironegoro@gmail.com>
2017-02-05 11:01:41 +0000

    [HP] Added shader faders.

---
+++ ICHack17/Assets/Scripts/Fader.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fader : MonoBehaviour {
+    public static Fader instance;
+
+    private Renderer fader;
+    public float totalTime;
+    public float idleTime;
+    private bool switching;
+
+    private float transTimer;
+    private float fadeTime;
+
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+        instance = this;
+        fadeTime = (totalTime - idleTime) / 2;
+        fader = GetComponentInChildren<Renderer>();
+    }
+
+    void Update() {
+        if (!switching) {
+            return;
+        }
+
+        if (transTimer > 0f) {
+            transTimer -= Time.deltaTime;
+        }
+        if (transTimer < 0f) {
+            transTimer = 0f;
+        }
+
+        float cutoff = transTimer > (totalTime - fadeTime) ? (1 - (transTimer - idleTime - fadeTime) / fadeTime) :
+            (transTimer > fadeTime ? 1.0f : (transTimer / fadeTime));
+
+
+        if (switching) {
+            fader.material.SetFloat("_Cutoff", cutoff);
+        }
+
+        if (transTimer <= 0f) {
+            switching = false;
+        }
+    }
+
+    public void StartEffect(Texture tex) {
+        switching = true;
+        transTimer = totalTime;
+        fader.material.SetTexture("_TransitionTex", tex);
+    }
+}
--- ICHack17/Assets/Scripts/SceneSwitch.cs
+++ ICHack17/Assets/Scripts/SceneSwitch.cs
@@ -1,15 +1,49 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
- 
+
 public class SceneSwitch : MonoBehaviour {
     public string nextSceneName;
     public bool serverSwitch;
+    public Texture cutoffTex;
+
+    private float fadeTime = 1.0f;
+    private float switchTimer;
+    private bool switching;
+
+    void Update() {
+        if (!switching) {
+            return;
+        }
+
+        if (fadeTime > 0) {
+            fadeTime -= Time.deltaTime;
+        } else {
+            fadeTime = 0;
+            if (serverSwitch) {
+                NetworkSceneSwitcher.instance.Switch(nextSceneName);
+            } else {
+                SceneManager.LoadScene(nextSceneName);
+            }
+            switching = false;
+        }
+    }
+
+    public void NextScene(string nextSceneName) {
+        if (serverSwitch && !NetworkSceneSwitcher.instance) {
+            return;
+        }
+        this.nextSceneName = nextSceneName;
+        Fader.instance.StartEffect(cutoffTex);
+        switching = true;
+        switchTimer = fadeTime;
+    }
 
     public void NextScene() {
-        if(serverSwitch) {
-            NetworkSceneSwitcher.instance.Switch(nextSceneName);
-        } else {
-            SceneManager.LoadScene(nextSceneName);
+        if (serverSwitch && !NetworkSceneSwitcher.instance) {
+            return;
         }
+        Fader.instance.StartEffect(cutoffTex);
+        switching = true;
+        switchTimer = fadeTime;
     }
 }
