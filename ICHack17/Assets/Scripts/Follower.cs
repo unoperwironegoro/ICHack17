4e3280d2ce655d3fd1b6a415cb742acbe9d103bc
Nimesh Subedi <Nimesh Subedi>
2017-02-05 04:35:46 +0000

    [NS] Recreated sumo branch.

---
+++ ICHack17/Assets/Scripts/Follower.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Follower : NetworkBehaviour {
+
+    public float speed;
+    public Transform mouse { set; private get; }
+
+    /* private void OnTriggerEnter2D(Collider2D collision)
+     {
+
+         if (collision.gameObject.CompareTag("Player"))
+         {
+             collision.gameObject.SetActive(false);
+         }
+     } */
+
+    void Update()
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        float step = speed * Time.deltaTime;
+
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10.0f;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        if (mousePos.x < 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        } else
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        Vector3 playerPos = transform.position;
+
+        if (Vector3.Distance(mousePos, playerPos) < 2)
+        {
+            step /= 2;
+        }
+
+        transform.position = Vector3.MoveTowards(playerPos, mousePos, step);
+    }
+}
