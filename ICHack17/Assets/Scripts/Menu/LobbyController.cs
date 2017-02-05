ff00cd6302b2146dc5856dfe6e4e7c69281633d6
Harjuno Perwironegoro <hperwironegoro@gmail.com>
2017-02-05 02:00:36 +0000

    [HP] Implemented server scene switching.

---
+++ ICHack17/Assets/Scripts/Menu/LobbyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LobbyController : NetworkBehaviour {
+    public GameObject[] prefabs;
+
+    void Awake() {
+    }
+
+	// Use this for initialization
+	void Start () {
+    }
+}
