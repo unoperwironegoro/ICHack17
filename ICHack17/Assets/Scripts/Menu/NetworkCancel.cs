fa278ff54d73d45b246ff89bd46732e42b3e2f77
Harjuno Perwironegoro <hperwironegoro@gmail.com>
2017-02-08 04:04:21 +0000

    [HP] Implemented connection cancellation, tidied some of the centre panels.

---
+++ ICHack17/Assets/Scripts/Menu/NetworkCancel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkCancel : MonoBehaviour {
+    public void CancelConnection() {
+        NetworkCalls.nm.StopClient();
+    }
+}
